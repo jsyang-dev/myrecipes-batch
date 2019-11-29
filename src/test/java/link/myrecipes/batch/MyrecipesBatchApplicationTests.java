8a353de7c03f74de3952440f2b6e14d2c9b2445d
Jeongsu Yang <mycat83@gmail.com>
2019-11-29 23:31:35 +0900

    Initial commit

---
+++ src/test/java/link/myrecipes/batch/MyrecipesBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package link.myrecipes.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrecipesBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
